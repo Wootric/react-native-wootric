fde15ce202d1be6cd7e5f9050cf8a57baad8c27a
Sacha Spinelli <sacha@ripio.com>
2021-02-08 15:40:57 -0300

    fix wootric rn crashes

--- android/src/main/java/com/reactlibrary/RNWootricPackage.java
+++ android/src/main/java/com/reactlibrary/RNWootricPackage.java
@@ -6,20 +6,26 @@ import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class RNWootricPackage implements ReactPackage {
-  @Override
-  public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-    return Arrays.<NativeModule>asList(new RNWootricModule(reactContext));
-  }
-
   // Deprecated from RN 0.47
   public List<Class<? extends JavaScriptModule>> createJSModules() {
     return Collections.emptyList();
   }
+
+  @Override
+  public List<NativeModule> createNativeModules(
+    ReactApplicationContext reactContext) {
+    List<NativeModule> modules = new ArrayList<>();
+
+    modules.add(new RNWootricModule(reactContext));
+
+    return modules;
+  }
+
 
   @Override
   public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
